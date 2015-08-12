653d6678a5d5380b4376578480a4b306e6fd17c5
Niraj Tripathi <nirajtrip@yahoo.com>
2015-08-12 15:30:41 -0400

    Create DBOperations.java

---
+++ mule-test1-soap/src/main/java/com/myorg/binding/DBOperations.java
@@ -0,0 +1,9 @@
+package com.myorg.binding;
+
+public interface DBOperations {
+	public String create(String json);
+	public String read(String json);
+	public String update(String json);
+	public String delete(String json);
+	  
+}
